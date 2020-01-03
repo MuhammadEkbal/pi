e5735b6ed7b9f63ab09c086f00d3cd2996bc5a11
muhammad.ekbal.ahmad@uni-oldenburg.de <muhammad.ekbal.ahmad@uni-oldenburg.de>
2020-01-03 15:47:18 +0100

    add flex,cdk add main component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  printer() {
+    const input = 'PRINTER = b?doc.print!doc.PRINTER\n' +
+      'SERVER = a!b.SERVER\n' +
+      'CLIENT = a?p.p!d.CLIENT\n' +
+      'new(a).new(b).(CLIENT|SERVER|PRINTER)';
+    // @ts-ignore
+    document.getElementById('code_input').value = input;
+  }
+}
